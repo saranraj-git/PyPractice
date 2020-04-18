28eac14e03336b316c8ff9ee798ffef06a4e6206
saran <47510467+svenugopal333@users.noreply.github.com>
2020-04-18 21:20:35 +1000

    FileCheck.py added

--- FileCheck.py
+++ FileCheck.py
@@ -1,13 +1,12 @@
-
-def IsFileExists(filepath):
-    try:
-        with open(filepath) as f:
-            return True
-    except IOError:
-        return False
-
+import os
+import re
+'''
+    os.path.exists(path) - Returns true if the path is a file, directory, or a valid symlink.
+    os.path.isfile(path) - Returns true if the path is a regular file or a symlink to a file.
+    os.path.isdir(path) - Returns true if the path is a directory or a symlink to a directory.
+'''
 def CheckStringInFile(filename,searchstring):
-    if IsFileExists(filename):
+    if os.path.exists(filename) and os.path.isfile(filename):
         print("{} exists".format(filename))
         with open(filename) as f:
             linenum = 1
@@ -16,6 +15,6 @@ def CheckStringInFile(filename,searchstring):
                     print("String {} found in Line number {}".format(searchstring,linenum))
                 linenum += 1
     else:
-        print("{} Not Exists".format(filename))
+        print("{} Not Exists or It may not be a regular file".format(filename))
 
-CheckStringInFile("testfile.txt","comment")
+CheckStringInFile("testfile.txt","cool")
